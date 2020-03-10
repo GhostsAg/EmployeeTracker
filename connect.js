c25e72c251c4b936a31e041859812a047f0e9247
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-09 20:35:37 -0500

    dept handle

--- connect.js
+++ connect.js
@@ -13,6 +13,47 @@ const connection = mysql.createConnection({
 connection.connect( (err) => {
     if (err) throw err;
     console.log(`Connected as id: ${connection.threadId}.\n`);
-
+    selectAct();
 });
 
+function selectAct() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Add Department",
+                "Add Employee Role",
+                "Add Employee"
+            ]
+        }
+    ]).then( ( {action} ) => {
+        switch (action) {
+            case "Add Department":
+                handleDept();
+                break;
+            default:
+                console.log("error");
+        }
+    });
+}
+
+function insertHandle(table, inputVal) {
+    connection.query(`INSERT INTO ${table} (dept_name) VALUES ("${inputVal}")`,
+    (err, res) => {
+        if (err) throw err;
+        console.log(`${inputVal} added to ${table}`);
+    });
+}
+
+function handleDept() {
+    inquirer.prompt([
+        {
+            name: "inputVal",
+            message: "Type in the name of the department you would like to add."
+        }
+    ]).then( ({ inputVal }) => {
+        insertHandle("department", inputVal);
+    });
+}
